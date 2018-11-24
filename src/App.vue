fbc3eca13b0eaa26ad45ed85219b0b4dde581581
Klara Fleischmann <fleischmann.kl@gmail.com>
2018-11-24 17:14:08 +0100

    refactor form to component MovieForm

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,7 @@
 <div id="app">
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Movlix 🎥" />
-  <div class="movieForm">
-    <fieldset>
-      <label for="movie-title">Title</label>
-      <input type="text" name="movie-title" v-model="newMovie.title" placeholder="">
-    </fieldset>
-    <fieldset>
-      <label for="movie-year">Year</label>
-      <input type="number" name="movie-year" v-model="newMovie.year">
-    </fieldset>
-    <fieldset>
-      <label for="movie-length">Length (in min)</label>
-      <input type="number" name="movie-length" v-model="newMovie.length">
-    </fieldset>
-    <fieldset>
-      <label for="movie-rating">Rating (of 5)</label>
-      <input type="number" name="movie-rating" v-model="newMovie.rating">
-    </fieldset>
-    <fieldset>
-      <label for="movie-desc">Description</label>
-      <textarea name="movie-desc" rows="6" cols="60" v-model="newMovie.desc"></textarea>
-    </fieldset>
-    <button type="button" name="button" v-on:click="addMovie">Add Movie</button>
-  </div>
+  <MovieForm v-on:addMovie="addMovie" :movieData="newMovie" />
   <MovieCard v-for="(movie, index) in movies" :key="movie.id" :data-index="index" :movieData="movie" v-on:log="removeMovie" />
 </div>
 </template>
@@ -32,6 +10,7 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import MovieCard from './components/MovieCard.vue'
+import MovieForm from './components/MovieForm.vue'
 
 import movies from './assets/seedMovies.js'
 
@@ -51,7 +30,8 @@ export default {
   }),
   components: {
     HelloWorld,
-    MovieCard
+    MovieCard,
+    MovieForm
   },
   methods: {
     removeMovie: function(target) {
@@ -95,30 +75,4 @@ export default {
   padding: 0;
 }
 
-.movieForm {
-  width: 60vw;
-  background-color: rgba(0, 0, 0, 0.1);
-  padding: 1rem 2rem;
-  margin-bottom: 2rem;
-  border-radius: 0 0 5px 5px;
-}
-
-.movieForm fieldset {
-  border: none;
-  display: flex;
-  flex-direction: column;
-  padding: 0.5rem 0;
-  margin: 0;
-}
-
-.movieForm * {
-  width: 100%;
-  box-sizing: border-box;
-}
-
-.movieForm button {
-  margin: 0.5rem 0;
-  padding: 0.25rem;
-  text-transform: uppercase;
-}
 </style>
---
+++ src/components/MovieForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="movieForm">
+    <fieldset>
+      <label for="movie-title">Title</label>
+      <input type="text" name="movie-title" v-model="movieData.title" placeholder="">
+    </fieldset>
+    <fieldset>
+      <label for="movie-year">Year</label>
+      <input type="number" name="movie-year" v-model="movieData.year">
+    </fieldset>
+    <fieldset>
+      <label for="movie-length">Length (in min)</label>
+      <input type="number" name="movie-length" v-model="movieData.length">
+    </fieldset>
+    <fieldset>
+      <label for="movie-rating">Rating (of 5)</label>
+      <input type="number" name="movie-rating" v-model="movieData.rating">
+    </fieldset>
+    <fieldset>
+      <label for="movie-desc">Description</label>
+      <textarea name="movie-desc" rows="6" cols="60" v-model="movieData.desc"></textarea>
+    </fieldset>
+    <button type="button" name="button" v-on:click="$emit('addMovie')">Add Movie</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieForm',
+  props: {
+    type: String,
+    movieData: Object,
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.movieForm {
+  width: 60vw;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 1rem 2rem;
+  margin-bottom: 2rem;
+  border-radius: 0 0 5px 5px;
+}
+.movieForm fieldset {
+  border: none;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem 0;
+  margin: 0;
+}
+.movieForm * {
+  width: 100%;
+  box-sizing: border-box;
+}
+.movieForm button {
+  margin: 0.5rem 0;
+  padding: 0.25rem;
+  text-transform: uppercase;
+}
+</style>
